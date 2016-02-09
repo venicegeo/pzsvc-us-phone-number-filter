5ed53781f32aecddd77596edd9453808336131b9
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2016-02-09 18:38:31 -0500

    Made one single regular expression

--- us-phone-number-filter/grails-app/services/us/phone/number/filter/PhoneNumberFilterService.groovy
+++ us-phone-number-filter/grails-app/services/us/phone/number/filter/PhoneNumberFilterService.groovy
@@ -8,20 +8,11 @@ class PhoneNumberFilterService {
 
 
 	def checkForPhoneNumbers(object) {
-		def text = object.toString()
+		def pattern = /([^0-9]+[(]?[2-9]\d{2}[)]?|^[(]?[2-9]\d{2}[)]?)(\s|-|[.])[2-9]\d{2}(\s|-|[.])(\d{4}[^0-9]+|\d{4}$)/
 
-		def patterns = [
-			/1(\s|-|[.])[(][2-9]\d{2}[)](\s|-|[.])[2-9]\d{2}(\s|-|[.])(\d{4}[^0-9]+|\d{4}$)/,
-			/1(\s|-|[,])[2-9]\d{2}(\s|-|[.])[2-9]\d{2}(\s|-|[.])(\d{4}[^0-9]+|\d{4}$)/,
-			/[(][2-9]\d{2}[)](\s|-|[.])[2-9]\d{2}(\s|-|[.])(\d{4}[^0-2]+|\d{4}$)/,
-			/([^0-9]+[2-9]\d{2}|^[2-9]\d{2})(\s|-|[.])[2-9]\d{2}(\s|-|[.])(\d{4}[^0-9]+|\d{4}$)/
-		]
-
-
-		patterns.each() {
-			if (text.find(it)) { return true }
-		}
-		return false	
+		
+		if (object.toString().find(pattern)) { return true }
+		else { return false }
 	}
 
 	def serviceMethod(params, request) {
